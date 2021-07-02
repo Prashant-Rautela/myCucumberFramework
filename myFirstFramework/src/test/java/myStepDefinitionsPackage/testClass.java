90b86e661f1ba3a678c483443c4d4b24be9feda9
Prashant <rautela49@gmail.com>
2021-07-03 00:16:43 +0530

    comming my changes into my local

---
+++ myFirstFramework/src/test/java/myStepDefinitionsPackage/testClass.java
@@ -0,0 +1,17 @@
+package myStepDefinitionsPackage;
+
+import org.testng.annotations.Test;
+
+public class testClass {
+	
+	@Test
+	public void test() {
+		System.out.println("added with myBranch checkout");
+	}
+
+	
+	@Test
+	public void test1() {
+		System.out.println("another method added");
+	}
+}
